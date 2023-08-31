410dd407895424f37f0d96d6c623f7309e00f6a3
ohmypotato <mekeni07@gmail.com>
2023-08-31 09:47:45 +0800

    sample upload metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-import Image from 'next/image'
 import HeroSection from './components/HeroSection'
 import AboutSection from './components/AboutSection'
 import ProjectSection from './components/ProjectSection'
 import ContactSection from './components/ContactSection'
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'JKGC Portfolio',
+  description: 'Created and Designed by JKGC',
+}
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">

fdf64680544fd1032f42813cf9c79bb78f740152
janzenncruz5679 <mekeni07@gmail.com>
2025-07-22 12:45:08 +0800

    adjusted year

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
             <hr className='w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0'></hr>
             <div className='mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between'>
                 <div className='flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100'>
-                    © 2023 JKGC<Link href='/' className='hover:underline'></Link>
+                    © 2025 JKGC<Link href='/' className='hover:underline'></Link>
                 </div>
                 <div className='flex flex-row items-center justify-center space-x-2 mb-1'>
                     <Link href='https://github.com/ohmypotato' rel='noreferrer' target='_blank'>
